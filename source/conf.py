86d0ee9596ab6fe17566d590afb56181056435af
tes <tes@oath.com>
2018-07-29 12:47:45 -0700

    Update css configuration

--- source/conf.py
+++ source/conf.py
@@ -107,10 +107,11 @@ html_theme_options = {'navbar_pagenav': False, 'navbar_links': [("Vim", "vim/ind
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+
 html_static_path = ['_static']
 
 def setup(app):
-    app.add_stylesheet("my-styles-css")
+    app.add_stylesheet('css/my-styles-css')
     # app.add_stylesheet("ANOTHER.css")
     # app.add_stylesheet("AND ANOTHER.css")
 
